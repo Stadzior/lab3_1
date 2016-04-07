037283d9c16137487c23a32711d5acdc93d25f7c
Stadzior <Stadzior@gmail.com>
2016-04-07 18:30:21 +0200

    First Try of mocking InvoiceRequest.

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -1,8 +1,28 @@
 package lab3_1;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import static org.mockito.Mockito.when;
 public class BookKeeperTest {
 	@Test
 	public void RequestForSinglePositionInvoiceShouldReturnIt(){
 		
+		//Arrange
+		InvoiceFactory invoicer = Mockito.mock(InvoiceFactory.class);
+		InvoiceRequest request = Mockito.mock(InvoiceRequest.class);
+		ClientData client = Mockito.mock(ClientData.class);
+		Invoice invoice = Mockito.mock(Invoice.class);
+		//Act
+		
+		when(invoicer.create(client)).thenReturn(invoice);
+		//when(request.)
+		//Assert
+		//assertThat(actual, matcher);
 	}
 }
