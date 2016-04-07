20315c172d997002f14df02dd8763b75b365ed21
Stadzior <Stadzior@gmail.com>
2016-04-07 18:54:12 +0200

    Struggle to arrange states.

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -7,8 +7,10 @@ import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceLine;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
 import static org.mockito.Mockito.when;
+
 public class BookKeeperTest {
 	@Test
 	public void RequestForSinglePositionInvoiceShouldReturnIt(){
@@ -18,9 +20,11 @@ public class BookKeeperTest {
 		InvoiceRequest request = Mockito.mock(InvoiceRequest.class);
 		ClientData client = Mockito.mock(ClientData.class);
 		Invoice invoice = Mockito.mock(Invoice.class);
+		InvoiceLine invoiceLine = Mockito.mock(InvoiceLine.class);
+		
 		//Act
 		
-		when(invoicer.create(client)).thenReturn(invoice);
+		invoicer.create(client);
 		//when(request.)
 		//Assert
 		//assertThat(actual, matcher);
