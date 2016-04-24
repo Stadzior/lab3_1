4071d72a00a0641f3a78e6dd1f710b061ce98010
Stadzior <Stadzior@gmail.com>
2016-04-24 17:53:19 +0200

    Added test class AddProductCommandHandlerTest, with empty test methods.

---
+++ src/test/java/AddProductCommandHandlerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Kamil on 2016-04-24.
+ */
+public class AddProductCommandHandlerTest {
+
+    @Before
+    public void initialize(){
+
+    }
+
+    @Test
+    public void ShouldCallSuggestEquivalentOnce_WhenProductIsNotAvailable(){
+
+    }
+
+    @Test
+    public void ShouldCallReservationLoadAndSaveOnce(){
+
+    }
+
+    @Test
+    public void IsProductAddedToReservationRepository_WhenItIsAvailable(){
+        
+    }
+}
