ffbd5a4ab366ffd0fe88647b483cdf695d4d76f8
Stadzior <Stadzior@gmail.com>
2016-04-24 13:44:08 +0200

    Test Case: ShouldReturnSinglePositionInvoice_WhenRequestedSinglePositionInvoice

---
+++ src/test/java/BookKeeperTest.java
@@ -0,0 +1,57 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+
+import java.util.Date;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.when;
+
+public class BookKeeperTest {
+    InvoiceFactory invoicer;
+    InvoiceRequest request;
+    TaxPolicy policy;
+    ClientData client;
+    BookKeeper bookKeeper;
+    ProductData data;
+    RequestItem item;
+
+    @Before
+    public void initialize(){
+        Money money = new Money(1);
+        invoicer = Mockito.mock(InvoiceFactory.class);
+        client = new ClientData(Id.generate(),"John");
+        when(invoicer.create(client)).thenReturn(new Invoice(Id.generate(), client));
+
+        bookKeeper = new BookKeeper(invoicer);
+        data = new ProductData(Id.generate(),money,"Test", ProductType.DRUG,new Date());
+        item = new RequestItem(data,1,data.getPrice());
+        policy = Mockito.mock(TaxPolicy.class);
+        when(policy.calculateTax(data.getType(),data.getPrice())).thenReturn(new Tax(money, ""));
+
+        request = new InvoiceRequest(client);
+
+    }
+
+	@Test
+	public void ShouldReturnSinglePositionInvoice_WhenRequestedSinglePositionInvoice(){
+		
+		//Arrange
+        request.add(item);
+		
+		//Act
+		Invoice invoice = bookKeeper.issuance(request,policy);
+
+		//Assert
+        Assert.assertThat(invoice.getItems().size(),equalTo(1));
+	}
+}
