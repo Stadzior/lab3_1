d6583d479b27e2a43bf1694f1b60ad2ecd3e7442
Stadzior <Stadzior@gmail.com>
2016-04-24 12:25:15 +0200

    Tiding mocking mess.

--- src/test/java/lab3_1/BookKeeperTest.java
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -1,26 +1,37 @@
 package lab3_1;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
-import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
-import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
-import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceLine;
-import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
+
 import static org.mockito.Mockito.when;
 
 public class BookKeeperTest {
+    InvoiceFactory invoicer;
+    InvoiceRequest request;
+    TaxPolicy policy;
+    ClientData client;
+    Invoice invoice;
+    InvoiceLine invoiceLine;
+    @Before
+    public void initialize(){
+        invoicer = Mockito.mock(InvoiceFactory.class);
+        request = Mockito.mock(InvoiceRequest.class);
+        policy = Mockito.mock(TaxPolicy.class);
+        client = Mockito.mock(ClientData.class);
+        invoice = Mockito.mock(Invoice.class);
+        invoiceLine = Mockito.mock(InvoiceLine.class);
+    }
 	@Test
-	public void RequestForSinglePositionInvoiceShouldReturnIt(){
+	public void ShouldReturnSinglePositionInvoice_WhenRequestedSinglePositionInvoice(){
 		
 		//Arrange
-		InvoiceFactory invoicer = Mockito.mock(InvoiceFactory.class);
-		InvoiceRequest request = Mockito.mock(InvoiceRequest.class);
-		ClientData client = Mockito.mock(ClientData.class);
-		Invoice invoice = Mockito.mock(Invoice.class);
-		InvoiceLine invoiceLine = Mockito.mock(InvoiceLine.class);
+
+
 		
 		//Act
 		
